d231aa199cec12146a11b0806c3019eeb5f5d165
OthmanHaba <othman.hadi2002@gmail.com>
2025-02-28 16:43:29 +0200

    Active request lockup

--- app/Http/Controllers/API/ServiceController.php
+++ app/Http/Controllers/API/ServiceController.php
@@ -19,7 +19,7 @@ class ServiceController extends Controller
     public function lockUp(Request $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [
-            'service_id' => 'required|exists:'.ServicType::class.',id',
+            'service_id' => 'required|exists:' . ServicType::class . ',id',
             'coordinate' => 'required|array',
             'coordinate.latitude' => 'required|numeric',
             'coordinate.longitude' => 'required|numeric',
@@ -29,26 +29,27 @@ class ServiceController extends Controller
             return response()->json($validator->errors(), 401);
         }
 
-       $availableProviderWithService = User::whereRole('provider')
-           ->whereisActive( true)
-           ->whereHas('providerServices', function (Builder $query) use ($request) {
-               $query->where('servic_type_id', $request->service_id);
-           })
-           ->whereHas('currentLocation',function($q) use ($request) {
-           })
-           ->with('currentLocation')
-           ->doesntHave('providerActiveRequests')
-           ->get();
+        $availableProviderWithService = User::whereRole('provider')
+            ->whereisActive(true)
+            ->whereHas('providerServices', function (Builder $query) use ($request) {
+                $query->where('servic_type_id', $request->service_id);
+            })
+            ->whereHas('currentLocation', function ($q) use ($request) {
+                //TODO: add the distance filter
+            })
+            ->with('currentLocation')
+            ->doesntHave('providerActiveRequests')
+            ->get();
 
-        if($availableProviderWithService->isEmpty()){
+        if ($availableProviderWithService->isEmpty()) {
             return response()->json([
                 'message' => 'No provider services available',
-            ],ResponseCode::NoContent->value);
+            ], ResponseCode::NoContent->value);
         }
 
         $provider = $availableProviderWithService->first();
 
-        $activeRequest = ActiveRequest::create([
+        ActiveRequest::create([
             'user_id' => auth()->id(),
             'provider_id' => $provider->id,
             'price' => $provider
@@ -57,10 +58,13 @@ class ServiceController extends Controller
             'status' => ServiceStatus::PendingUserApproved,
         ]);
 
+        $activeRequest = ActiveRequest::latest()->first();
+
         //TODO notify the provider
 
         return response()->json([
-            'providers' => $availableProviderWithService,
-        ],ResponseCode::Success->value);
+            'provider' => $provider,
+            'active_request' => $activeRequest,
+        ], ResponseCode::Success->value);
     }
 }
