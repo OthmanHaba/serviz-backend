7d523d8be8fd53c73cf2d65d0f7faefe1613c39e
Othman Haba <124089971+OthmanHaba@users.noreply.github.com>
2025-04-05 17:21:25 +0200

    new fix

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return to_route('/admin');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
