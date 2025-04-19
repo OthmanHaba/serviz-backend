e79618f4f85b3d7e3b3841309e286e85fc963242
OthmanHaba <othman.hadi2002@gmail.com>
2025-04-19 03:13:47 +0200

    change route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return to_route('filament.home');
+    return to_route('filament.admin.pages.dashboard');
 });
 
 require __DIR__.'/auth.php';
