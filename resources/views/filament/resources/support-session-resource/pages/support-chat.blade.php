4940e638dc936a0b4f3f2b0a3b5f857f14b8796a
OthmanHaba <othman.hadi2002@gmail.com>
2025-04-20 18:32:15 +0200

    fix

--- resources/views/filament/resources/support-session-resource/pages/support-chat.blade.php
+++ resources/views/filament/resources/support-session-resource/pages/support-chat.blade.php
@@ -6,19 +6,19 @@
                     <div
                         class="flex-1 {{ $msg->sender->role === 'admin' ? 'ml-auto' : '' }}">
 
-                        <div class="bg-white rounded-lg p-4 shadow-md">
-                            <p class="text-sm text-gray-800">{{ $msg->message }}</p>
-                            <small class="text-xs text-gray-500">{{ $msg->created_at->diffForHumans() }}</small>
-                            <strong class="text-xs text-blue-200">from : {{$msg->sender->name }}</strong>
+                        <div class="bg-white dark:bg-gray-800 rounded-lg p-4 shadow-md">
+                            <p class="text-sm text-gray-800 dark:text-gray-200">{{ $msg->message }}</p>
+                            <small class="text-xs text-gray-500 dark:text-gray-400">{{ $msg->created_at->diffForHumans() }}</small>
+                            <strong class="text-xs text-blue-200 dark:text-blue-400">from : {{$msg->sender->name }}</strong>
                         </div>
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
-    <div class="flex items-center p-4 bg-gray-100">
+    <div class="flex items-center p-4 bg-gray-100 dark:bg-gray-900">
         <x-filament::input wire:model="newMessage" placeholder="Type a message..."
-                           class="w-full px-4 py-2 text-sm text-gray-700 bg-white border rounded-md focus:outline-none focus:ring focus:border-blue-300"/>
+                           class="w-full px-4 py-2 text-sm text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-800 border rounded-md focus:outline-none focus:ring focus:border-blue-300 dark:focus:border-blue-500"/>
         <x-filament::button wire:click="sendNewMessage" class="ml-4">Send</x-filament::button>
     </div>
     <x-filament::button wire:click="setIsClosed" class="mt-4" color="danger">Close Support</x-filament::button>
